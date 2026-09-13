2c677b61050157dc6f22c0e433a4489d3420beaf
yuurkoo <urabuchko10@gmail.com>
2026-09-13 16:45:07 +0300

    chore: centralize metadata configuration

--- apps/client/src/app/[locale]/layout.tsx
+++ apps/client/src/app/[locale]/layout.tsx
@@ -1,13 +1,11 @@
 import { NextIntlClientProvider } from "next-intl";
 import { routing } from "@/i18n/routing";
+import { metadataConfig } from "@/libs/config";
 
 import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {
-  title: "",
-  description: "",
-};
+export const metadata: Metadata = metadataConfig;
 
 export default function RootLayout({ children }: LayoutProps<"/[locale]">) {
   return (
--- apps/client/src/libs/config/index.ts
+++ apps/client/src/libs/config/index.ts
@@ -1,1 +1,2 @@
 export { env } from "./env";
+export { metadataConfig } from "./metadata";
