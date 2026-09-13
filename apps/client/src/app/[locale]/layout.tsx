730c6e7db0d725cc3d4c5deeac8107c4e5b67494
yuurkoo <urabuchko10@gmail.com>
2026-09-13 17:07:14 +0300

    chore: set up React Query

--- apps/client/src/app/[locale]/layout.tsx
+++ apps/client/src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import { NextIntlClientProvider } from "next-intl";
 import { routing } from "@/i18n/routing";
 import { metadataConfig } from "@/libs/config";
+import { QueryProvider } from "@/providers";
 
 import type { Metadata } from "next";
 import "./globals.css";
@@ -11,7 +12,9 @@ export default function RootLayout({ children }: LayoutProps<"/[locale]">) {
   return (
     <html lang="en">
       <body>
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+        <NextIntlClientProvider>
+          <QueryProvider>{children}</QueryProvider>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
